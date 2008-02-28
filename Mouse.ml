e49f40e5e2342107dff7ad88f8b43f26e1ae9bc7
Glenn Y. Rolland <glenux@glenux.net>
2008-02-28 17:48:52 +0100

    Added ocamldoc comments.

--- Mouse.ml
+++ Mouse.ml
@@ -1,4 +1,10 @@
 
+(** Mouse management and event handling. *)
+(* vim: set ts=2 sw=2 et : *)
+
+(** Returns a list of "abstract events" generated 
+    by this SDL event *)
 let handle_event ev =
-    ignore ev
+  ignore ev ; []
 ;;
+
